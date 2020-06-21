e6de8ad4ce9ce1f10f6bb47fcc1a58c9596db8cf
Julita Marshall <julitamcg5@gmail.com>
2020-06-21 06:32:05 -0500

    initial

---
+++ drf_shoestore_frontend/frontend/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      shoedata: []
+    }
+  }
+  componentDidMount() {
+    fetch('http://127.0.0.1:8000/api/shoe/')
+      .then(res => res.json())
+      .then(data => this.setState({ shoedata: data }))
+  }
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.shoedata.map(shoe => {
+            return (
+              <li key={shoe.id}>
+              -size: {shoe.size}<br></br>
+              -brand: {shoe.brand_name}<br></br>
+              -manufacturer: {shoe.manufacturer}<br></br>
+              -color: {shoe.color}<br></br>
+              -material: {shoe.material}<br></br>
+              -type: {shoe.shoe_type}<br></br>
+              -fastener: {shoe.fasten_type}<br></br>
+              </li>
+            )
+
+        })}    
+        </ul>
+      </div> 
+    );
+  }
+
+}
+
+export default App;
