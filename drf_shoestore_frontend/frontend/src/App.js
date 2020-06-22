f869093ff84ad17b31ef2b790bc3bd01e8a748b0
Julita Marshall <julitamcg5@gmail.com>
2020-06-21 20:02:15 -0500

    correct app.js for data to show

--- drf_shoestore_frontend/frontend/src/App.js
+++ drf_shoestore_frontend/frontend/src/App.js
@@ -1,42 +1,53 @@
-import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      shoedata: []
-    }
+      shoedata: [],
+    };
   }
   componentDidMount() {
-    fetch('http://127.0.0.1:8000/api/shoe/')
+    fetch("http://127.0.0.1:8000/api/shoe/")
       .then(res => res.json())
-      .then(data => this.setState({ shoedata: data }))
+      // .then((res) => console.log(res))
+      .then((data) => this.setState({ shoedata: data }));
   }
+
   render() {
     return (
       <div>
+      <h4>Shoes</h4>
         <ul>
-          {this.state.shoedata.map(shoe => {
+          {this.state.shoedata && this.state.shoedata.map((shoe, index) => {
             return (
-              <li key={shoe.id}>
-              -size: {shoe.size}<br></br>
-              -brand: {shoe.brand_name}<br></br>
-              -manufacturer: {shoe.manufacturer}<br></br>
-              -color: {shoe.color}<br></br>
-              -material: {shoe.material}<br></br>
-              -type: {shoe.shoe_type}<br></br>
-              -fastener: {shoe.fasten_type}<br></br>
+              <li key={index}>
+                {" "}
+                key={shoe.id}
+                <br></br>
+                size: {shoe.size}
+                <br></br>
+                brand: {shoe.brand_name}
+                <br></br>
+                manufacturer: {shoe.manufacturer}
+                <br></br>
+                color: {shoe.color}
+                <br></br>
+                material: {shoe.material}
+                <br></br>
+                type: {shoe.shoe_type}
+                <br></br>
+                fastener: {shoe.fasten_type}
+                <br></br>
               </li>
-            )
-
-        })}    
+            );
+          })}
         </ul>
-      </div> 
+      </div>
     );
   }
-
 }
 
 export default App;
