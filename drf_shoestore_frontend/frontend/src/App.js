47d00b651658b6e62e16314ef948e7e36d403eca
Julita Marshall <julitamcg5@gmail.com>
2020-07-02 16:14:27 -0500

    files removed

--- drf_shoestore_frontend/frontend/src/App.js
+++ drf_shoestore_frontend/frontend/src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+// Thanks Matt for assisting me with my bootstrap and react!
+
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -12,38 +15,36 @@ class App extends React.Component {
   componentDidMount() {
     fetch("http://127.0.0.1:8000/api/shoe/")
       .then(res => res.json())
-      // .then((res) => console.log(res))
       .then((data) => this.setState({ shoedata: data }));
   }
 
   render() {
+    let output=this.state.shoedata.map((shoe) => (           
+        <li>
+          key={shoe.id}
+          <br></br>
+          size: {shoe.size}
+          <br></br>
+          brand: {shoe.brand_name}
+          <br></br>
+          manufacturer: {shoe.manufacturer}
+          <br></br>
+          color: {shoe.color}
+          <br></br>
+          material: {shoe.material}
+          <br></br>
+          type: {shoe.shoe_type}
+          <br></br>
+          fastener: {shoe.fasten_type}
+          <br></br>
+        </li>
+    ));
+      
     return (
       <div>
       <h4>Shoes</h4>
         <ul>
-          {this.state.shoedata && this.state.shoedata.map((shoe, index) => {
-            return (
-              <li key={index}>
-                {" "}
-                key={shoe.id}
-                <br></br>
-                size: {shoe.size}
-                <br></br>
-                brand: {shoe.brand_name}
-                <br></br>
-                manufacturer: {shoe.manufacturer}
-                <br></br>
-                color: {shoe.color}
-                <br></br>
-                material: {shoe.material}
-                <br></br>
-                type: {shoe.shoe_type}
-                <br></br>
-                fastener: {shoe.fasten_type}
-                <br></br>
-              </li>
-            );
-          })}
+          { output }
         </ul>
       </div>
     );
